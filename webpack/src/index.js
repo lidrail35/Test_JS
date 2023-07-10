5a5f08526794f882a0568cc88f0f5f6a11799c54
lidrail35 <lidrail35@mail.ru>
2023-07-10 10:31:43 +0300

    feat: change index.js

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,4 +1,12 @@
-import './index.html';
-import './style.css';
+// import './index.html';
+// import './style.css';
 
-console.log('Hello, world');
+// console.log('Hello, world');
+
+function showElement(text) {
+  const element = document.createElement('h2');
+  element.textContent = text;
+  return element;
+}
+
+document.body.append(showElement('Hello, world and webpack!'));
