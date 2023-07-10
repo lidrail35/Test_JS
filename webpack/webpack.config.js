e2aa6d7a7a6066bbba132ee580e7c2cb507b64a5
lidrail35 <lidrail35@mail.ru>
2023-07-10 11:12:15 +0300

    feat: change output point

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,4 +2,9 @@ const path = require('path');
 
 module.exports = {
   entry: path.join(__dirname, 'src', 'main.js'),
+  output: {
+    clean: true,
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
 };
