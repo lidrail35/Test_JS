8ca9d2e765ac7006fb372042169a1eace732723a
lidrail35 <lidrail35@mail.ru>
2023-07-10 14:22:34 +0300

    feat: add devServer

--- webpack/install_webpack.md
+++ webpack/install_webpack.md
@@ -1,4 +1,4 @@
 npm init -y
 npm install webpack webpack-cli --save-dev
 npm i -D html-webpack-plugin
-
+npm i -D webpack-dev-server
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,6 +2,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+  devServer: {
+    watchFiles: path.join(__dirname, 'src'),
+    open: true,
+    hot: true,
+    port: 9000,
+  },
   entry: path.join(__dirname, 'src', 'main.js'),
   output: {
     clean: true,
